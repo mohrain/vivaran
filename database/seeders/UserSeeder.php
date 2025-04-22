e4ada70a3ad4218226df35292679e2ce0077f96f
pr0vin <pravinchaudhary688@gmail.com>
2025-04-22 11:18:14 +0545

    Add authentication, routing, and testing setup
    - Created auth routes for registration, login, password reset, and email verification.
    - Added console command for displaying inspiring quotes.
    - Established web routes for the main application and profile management.
    - Implemented .gitignore files for various storage directories to exclude unnecessary files.
    - Configured Tailwind CSS with custom font settings.
    - Developed feature tests for authentication, email verification, password confirmation, password reset, and profile management.
    - Set up Pest testing framework with custom expectations and test case structure.
    - Initialized Vite configuration for asset management.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        $role = Role::findOrCreate('super-admin');
+        $user = User::create([
+            'name' => 'Super Admin',
+            'email' => 'prvn@gmail.com',
+            'password' => '$2y$12$cH3UK/yMotIT0kcFec4NnOImAlinQVO1yrNBqchugxfDp75rQK68O',
+        ]);
+
+        $user->assignRole($role);
+
+        $role = Role::findOrCreate('admin');
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'vivaran.admin@gmail.com',
+            'password' => '$2y$12$cH3UK/yMotIT0kcFec4NnOImAlinQVO1yrNBqchugxfDp75rQK68O',
+        ]);
+
+        $user->assignRole($role);
+    }
+}
